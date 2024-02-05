33bb26aca536eaacffe57922749390024a802421
ag <ag@PROELE64>
2024-02-05 12:42:15 +0100

    Changes

--- 2024-02-05_Json-Quiz-Create/Program.cs
+++ 2024-02-05_Json-Quiz-Create/Program.cs
@@ -1,41 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using Newtonsoft.Json.Converters;
-using System.IO;
-class Program
+﻿class Program
 {
     static void Main()
     {
         for (int i = 1; i < 5;)
         {
-            string filePath = (@"Questions/question.json");
-            //string filePath = (@"Questions/question{0}.json", i);
-            Convert_cs_to_json(filePath);
+            string filePath = (@"Questions/Question.json");
+            //string filePath = (@"Questions/Question{0}.json", i.ToString());
+            Convert_object_to_json(filePath);
             i++;
         }
+        Console.Write("Complete!");
+        Console.ReadKey();
     }
-    static void Convert_cs_to_json(string filePath)
+    public class QuizQuestion()
     {
-        string path = filePath;
-        var question = quizQuestionCreate();
-        var jsonFormattedContent = Newtonsoft.Json.JsonConvert.SerializeObject(question);
-        System.IO.File.WriteAllText(path, jsonFormattedContent);
-    }
-    public class quizQuestion()
-    {
-        public string question {  get; set; }
+        public string question { get; set; }
         public string correctAnswer { get; set; }
         public string correctAnswerExplanation { get; set; }
     }
-
-    static quizQuestion quizQuestionCreate()
+    static QuizQuestion quizQuestionCreate()
     {
-        var q = new quizQuestion();
+        var q = new QuizQuestion();
         return q;
+    }
+    static void Convert_object_to_json(string filePath)
+    {
+        string path = filePath;
+        var question = quizQuestionCreate();
+        var jsonFormating = Newtonsoft.Json.JsonConvert.SerializeObject(question);
+        System.IO.File.WriteAllText(path, jsonFormating);
     }
 }
